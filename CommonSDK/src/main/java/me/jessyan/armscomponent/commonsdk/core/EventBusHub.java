11e72f7dedb2d7dd9018c268f03b9ab2a930b201
JessYan <jessyan@foxmail.com>
2018-03-30 18:06:16 +0800

    Create CommonSDK's EventBusHub

--- CommonSDK/src/main/java/me/jessyan/armscomponent/commonsdk/core/EventBusHub.java
+++ CommonSDK/src/main/java/me/jessyan/armscomponent/commonsdk/core/EventBusHub.java
@@ -17,14 +17,13 @@ package me.jessyan.armscomponent.commonsdk.core;
 
 /**
  * ================================================
- * CommonSDK 的 Constant 可以定义公用的常量, 比如关于业务的常量或者正则表达式, 每个组件的 Constant 可以定义组件自己的私有常量
+ * EventBusHub 用来定义 AndroidEventBus 的 Tag 字符串, 以组件名作为 Tag 前缀, 对每个组件的事件进行分组
+ * Tag 的命名规则为 组件名 + 页面名 + 动作
  *
- * Created by JessYan on 30/03/2018 17:32
+ * Created by JessYan on 30/03/2018 17:46
  * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
  * <a href="https://github.com/JessYanCoding">Follow me</a>
  * ================================================
  */
-public interface Constant {
-    //电话号码正则
-    String PHONE_REGULAR = "^1[3-9]\\d{9}$";
+public interface EventBusHub {
 }
