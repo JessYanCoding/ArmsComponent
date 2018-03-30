5dfa582068a4a3b88b7b9677bfc3fbb15219ceab
JessYan <jessyan@foxmail.com>
2018-03-30 18:19:51 +0800

    Create CommonSDK's RouterHub

---
+++ CommonSDK/src/main/java/me/jessyan/armscomponent/commonsdk/core/RouterHub.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2017 JessYan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.jessyan.armscomponent.commonsdk.core;
+
+/**
+ * ================================================
+ * RouterHub 用来定义路由器的路由地址, 以组件名作为前缀, 对每个组件的路由地址进行分组, 统一查看和管理所有分组的路由地址
+ * 路由地址的命名规则为 组件名 + 页面名
+ *
+ * Created by JessYan on 30/03/2018 18:07
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public interface RouterHub {
+}
