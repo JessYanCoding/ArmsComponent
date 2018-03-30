87d24368be48db17a7ef3ef12ac76fcfed8c3ebc
JessYan <jessyan@foxmail.com>
2018-03-30 17:19:38 +0800

    Create CommonSDK's utils

---
+++ CommonSDK/src/main/java/me/jessyan/armscomponent/commonsdk/utils/Utils.java
@@ -0,0 +1,14 @@
+package me.jessyan.armscomponent.commonsdk.utils;
+
+/**
+ * ================================================
+ * Created by JessYan on 30/03/2018 17:16
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public class Utils {
+    private Utils() {
+        throw new IllegalStateException("you can't instantiate me!");
+    }
+}
