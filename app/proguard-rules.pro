025bb8f6dad9548fa1aa213406752fb15e47437c
JessYan <jessyan@foxmail.com>
2018-03-30 13:59:09 +0800

    Initial commit

---
+++ app/proguard-rules.pro
@@ -0,0 +1,19 @@
+# Add project specific ProGuard rules here.
+# By default, the flags in this file are appended to flags specified
+# in /Users/jess/Library/Android/sdk/tools/proguard/proguard-android.txt
+# You can edit the include path and order by changing the proguardFiles
+# directive in build.gradle.
+#
+# For more details, see
+#   http://developer.android.com/guide/developing/tools/proguard.html
+
+# Add any project specific keep options here:
+
+# If your project uses WebView with JS, uncomment the following
+# and specify the fully qualified class name to the JavaScript interface
+# class:
+#-keepclassmembers class fqcn.of.javascript.interface.for.webview {
+#   public *;
+#}
+
+# 混淆规则在 arms moudule下的proguard-rules.pro中,混淆前先参阅https://github.com/JessYanCoding/MVPArms/wiki#1.5
---
+++ app/src/main/java/me/jessyan/armscomponent/app/di/module/UserModule.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2017 JessYan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.jessyan.armscomponent.app.di.module;
+
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.jess.arms.di.scope.ActivityScope;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dagger.Module;
+import dagger.Provides;
+import me.jessyan.armscomponent.app.mvp.contract.UserContract;
+import me.jessyan.armscomponent.app.mvp.model.UserModel;
+import me.jessyan.armscomponent.app.mvp.model.entity.User;
+import me.jessyan.armscomponent.app.mvp.ui.adapter.UserAdapter;
+
+/**
+ * ================================================
+ * 展示 Module 的用法
+ *
+ * @see <a href="https://github.com/JessYanCoding/MVPArms/wiki#2.4.5">Module wiki 官方文档</a>
+ * Created by JessYan on 09/04/2016 11:10
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+@Module
+public class UserModule {
+    private UserContract.View view;
+
+    /**
+     * 构建 UserModule 时,将 View 的实现类传进来,这样就可以提供 View 的实现类给 Presenter
+     *
+     * @param view
+     */
+    public UserModule(UserContract.View view) {
+        this.view = view;
+    }
+
+    @ActivityScope
+    @Provides
+    UserContract.View provideUserView() {
+        return this.view;
+    }
+
+    @ActivityScope
+    @Provides
+    UserContract.Model provideUserModel(UserModel model) {
+        return model;
+    }
+
+    @ActivityScope
+    @Provides
+    RxPermissions provideRxPermissions() {
+        return new RxPermissions(view.getActivity());
+    }
+
+    @ActivityScope
+    @Provides
+    RecyclerView.LayoutManager provideLayoutManager() {
+        return new GridLayoutManager(view.getActivity(), 2);
+    }
+
+    @ActivityScope
+    @Provides
+    List<User> provideUserList() {
+        return new ArrayList<>();
+    }
+
+    @ActivityScope
+    @Provides
+    RecyclerView.Adapter provideUserAdapter(List<User> list){
+        return new UserAdapter(list);
+    }
+}
