e23d6255e86ba73fec655b0f6d93b499307edcd4
JessYan <jessyan@foxmail.com>
2018-04-19 17:30:28 +0800

    dependency Lottie

---
+++ app/src/main/java/me/jessyan/armscomponent/app/mvp/ui/activity/MainActivity.java
@@ -0,0 +1,34 @@
+package me.jessyan.armscomponent.app.mvp.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.jess.arms.base.BaseActivity;
+import com.jess.arms.di.component.AppComponent;
+
+import me.jessyan.armscomponent.app.R;
+
+/**
+ * ================================================
+ * Created by JessYan on 19/04/2018 16:10
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public class MainActivity extends BaseActivity {
+    @Override
+    public void setupActivityComponent(@NonNull AppComponent appComponent) {
+
+    }
+
+    @Override
+    public int initView(@Nullable Bundle savedInstanceState) {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initData(@Nullable Bundle savedInstanceState) {
+
+    }
+}
