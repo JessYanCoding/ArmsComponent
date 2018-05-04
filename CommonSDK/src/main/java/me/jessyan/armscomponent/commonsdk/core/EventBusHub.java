ce31a9c95330db3a5fdbecfd8fcf324335ea404b
JessYan <jessyan@foxmail.com>
2018-05-04 11:27:30 +0800

    Improve RouterHub and EventBusHub

--- CommonSDK/src/main/java/me/jessyan/armscomponent/commonsdk/core/EventBusHub.java
+++ CommonSDK/src/main/java/me/jessyan/armscomponent/commonsdk/core/EventBusHub.java
@@ -17,10 +17,13 @@ package me.jessyan.armscomponent.commonsdk.core;
 
 /**
  * ================================================
+ * AndroidEventBus 作为本方案提供的另一种跨组件通信方式 (第一种是接口下沉, 在 app 的 MainActivity 中已展示, 通过 ARouter 实现)
+ * AndroidEventBus 比 greenrobot 的 EventBus 多了一个 Tag, 在组件化中更容定位和管理事件
+ * <p>
  * EventBusHub 用来定义 AndroidEventBus 的 Tag 字符串, 以组件名作为 Tag 前缀, 对每个组件的事件进行分组
  * Tag 的命名规则为 组件名 + 页面名 + 动作
  * 比如需要使用 AndroidEventBus 通知订单组件的订单详情页进行刷新, 可以将这个刷新方法的 Tag 命名为 "order/OrderDetailActivity/refresh"
- *
+ * <p>
  * Created by JessYan on 30/03/2018 17:46
  * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
  * <a href="https://github.com/JessYanCoding">Follow me</a>
