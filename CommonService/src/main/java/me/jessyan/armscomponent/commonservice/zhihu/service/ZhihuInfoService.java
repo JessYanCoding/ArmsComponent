138613317c5a1797c17534ab03c457eb1eabedcd
JessYan <jessyan@foxmail.com>
2018-05-07 17:52:07 +0800

    Improve CommonService's comments

--- CommonService/src/main/java/me/jessyan/armscomponent/commonservice/zhihu/service/ZhihuInfoService.java
+++ CommonService/src/main/java/me/jessyan/armscomponent/commonservice/zhihu/service/ZhihuInfoService.java
@@ -21,6 +21,9 @@ import me.jessyan.armscomponent.commonservice.zhihu.bean.ZhihuInfo;
 
 /**
  * ================================================
+ * 向外提供服务的接口, 在此接口中声明一些具有特定功能的方法提供给外部, 即可让一个组件与其他组件或宿主进行交互
+ *
+ * @see <a href="https://github.com/JessYanCoding/ArmsComponent/wiki#2.2.3.2">CommonService wiki 官方文档</a>
  * Created by JessYan on 2018/4/27 14:16
  * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
  * <a href="https://github.com/JessYanCoding">Follow me</a>
