52d70b2c1cc6e7bea2ff2469dd3644bf299019e0
JessYan <jessyan@foxmail.com>
2018-05-07 17:39:03 +0800

    Improve Anim's comments

--- CommonRes/src/main/java/me/jessyan/armscomponent/commonres/utils/Anim.java
+++ CommonRes/src/main/java/me/jessyan/armscomponent/commonres/utils/Anim.java
@@ -23,19 +23,21 @@ import me.jessyan.armscomponent.commonres.R;
 
 
 /**
+ * ================================================
  * 动画工具类
- * Created by jess on 27/04/2017 15:30
- * Contact with jess.yan.effort@gmail.com
+ * <p>
+ * Created by JessYan on 27/04/2017 21:40
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
  */
-
 public class Anim {
-
     public static void exit(Activity obj) {
-        obj.overridePendingTransition(R.anim.public_translate_left_to_center,R.anim.public_translate_center_to_right);
+        obj.overridePendingTransition(R.anim.public_translate_left_to_center, R.anim.public_translate_center_to_right);
     }
 
     public static void in(Activity obj) {
-        obj.overridePendingTransition(R.anim.public_translate_right_to_center,R.anim.public_translate_center_to_left);
+        obj.overridePendingTransition(R.anim.public_translate_right_to_center, R.anim.public_translate_center_to_left);
     }
 
     public static void cleanAnim(ImageView animView) {
@@ -45,5 +47,4 @@ public class Anim {
         animView.clearAnimation();
         animView.setVisibility(View.GONE);
     }
-
 }
